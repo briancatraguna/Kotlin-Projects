fca766392685d655b656edeae9d7260169754f5f
briancatraguna <briancatraguna@rocketmail.com>
2021-03-01 23:33:56 +0700

    Day 11 progress

---
+++ HackerRankSolutions/30DaysofCode/Day11_2DArrays.kt
@@ -0,0 +1,20 @@
+import java.util.Scanner
+import java.util.Collections.max
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    var matrix = ArrayList<IntArray>(6)
+    for (row in 0 until 6) {
+        var theRow = reader.nextLine()
+        var rowList = theRow.split(" ").toTypedArray().map { it.toInt() }
+    }
+}
+
+fun findHourglassMax(matrix:ArrayList<IntArray>):Int{
+    for (i in 0 until 4){
+        for (j in 0 until 4){
+            
+        }
+    }
+    return 0
+}
